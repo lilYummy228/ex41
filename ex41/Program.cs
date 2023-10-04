d265e1310c09f1fc11d4a6e644a9a238a94fe6bc
lilYummy228 <142146352+lilYummy228@users.noreply.github.com>
2023-10-04 12:11:09 +0500

    Добавьте файлы проекта.

---
+++ ex41/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex41
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player(1123, "elleeee", 10, true);
+            DataBase players = new DataBase(player);
+
+            players.ShowInfo();
+        }
+    }
+
+    class DataBase
+    {
+        public Player Player;
+
+        public DataBase(Player player)
+        {
+            Player = player;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"ID:{Player.ID}\nNickName: {Player.NickName}\nУровень: {Player.Level}\nФлаг: {Player.IsBannedFlag}");
+        }
+    }
+
+    class Player
+    {
+        public int ID;
+        public string NickName;
+        public int Level;
+        public bool IsBannedFlag;
+
+        public Player(int id, string nickName, int level, bool isBannedFlag)
+        {
+            ID = id;
+            NickName = nickName;
+            Level = level;
+            IsBannedFlag = isBannedFlag;
+        }
+    }
+}
